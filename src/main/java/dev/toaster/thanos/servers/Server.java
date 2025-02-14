82c7d36500c204060d357dfa28c5fb3d572d4685
MiTiX <bast.boulet@gmail.com>
2025-02-14 22:50:41 +0100

    refactor: record to class

--- src/main/java/dev/toaster/thanos/servers/Server.java
+++ src/main/java/dev/toaster/thanos/servers/Server.java
@@ -1,9 +1,48 @@
 package dev.toaster.thanos.servers;
 
-public record Server(String hostname, int port) {
+import java.net.URL;
+import java.time.Instant;
+
+public class Server {
+    private final String id;
+    private final String hostname;
+    private final int port;
+    private final URL url;
+    private Instant lastHealthCheck;
+
+    public Server(String id, URL url) {
+        this.id = id;
+        this.url = url;
+        this.hostname = this.url.getHost();
+        this.port = this.url.getPort();
+    }
+
+    public void updateLastHealthCheck() {
+        this.lastHealthCheck = Instant.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public Instant getLastHealthCheck() {
+        return lastHealthCheck;
+    }
 
     @Override
     public String toString() {
-        return this.hostname + ":" + this.port;
+        return this.url.toString();
     }
 }
