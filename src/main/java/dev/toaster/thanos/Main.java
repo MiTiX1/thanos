c6b096e8fba0f9ebe50cf60ab9d5e5ab7a622ab9
MiTiX <bast.boulet@gmail.com>
2025-02-14 22:53:20 +0100

    cleaning

--- src/main/java/dev/toaster/thanos/Main.java
+++ src/main/java/dev/toaster/thanos/Main.java
@@ -1,17 +1,11 @@
 package dev.toaster.thanos;
 
-import dev.toaster.thanos.loadbalancers.LoadBalancer;
-import dev.toaster.thanos.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-//        LoadBalancer loadBalancer = new LoadBalancer();
-//        loadBalancer.registerServer(new Server("127.0.0.1", 8080));
-//        loadBalancer.registerServer(new Server("127.0.0.1", 8081));e
-
         SpringApplication.run(Main.class, args);
     }
 }
